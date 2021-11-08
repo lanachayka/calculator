393fa582b884a21a418b194018adc243c0a94a25
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2021-11-08 16:34:13 +0200

    add components and build structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Screen from "./components/Screen/Screen";
+import ButtonBox from "./components/ButtonBox/ButtonBox";
+import Button from "./components/Button/Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
 
 function App() {
   return (
-    <div>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => {
+          return (
+            <Button
+              key={i}
+              className={btn === "=" ? "equals" : ""}
+              value={btn}
+              onClick={() => {
+                console.log(`${btn} clicked!`);
+              }}
+            />
+          );
+        })}
+      </ButtonBox>
+    </Wrapper>
   );
 }
 
