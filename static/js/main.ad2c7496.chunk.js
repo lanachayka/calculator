e8fa6c48787da8b38f1beb031d7c5b72dba06c78
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2021-11-08 15:27:17 +0200

    Updates

---
+++ static/js/main.ad2c7496.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcalculator=this.webpackJsonpcalculator||[]).push([[0],{10:function(t,c,e){"use strict";e.r(c);var n=e(1),r=e.n(n),o=e(3),a=e.n(o),s=(e(8),e(0));var i=function(){return Object(s.jsx)("div",{})};a.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(i,{})}),document.getElementById("root"))},8:function(t,c,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.ad2c7496.chunk.js.map
